f5e1a1171f5583709c77478e315aaf759b9a09e2
enesgunacti <gnctenes@gmail.com>
2024-06-22 21:42:41 +0300

    Singleton Design Patter 2 Examples Added

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() => 
+            {
+                var instance = UploadService.Instance(1); 
+            });
+
+            Thread t2 = new Thread(() => 
+            {
+                var instance = UploadService.Instance(2); 
+            });
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+        }
+    }
+}
---
+++ SingletonDesignPattern/UploadService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+     sealed class UploadService
+    {
+        private UploadService()
+        {
+            
+        }
+        public int Id { get; set; }
+        private static readonly object _lock = new object();
+
+        private static UploadService _instance;
+
+        public static UploadService Instance (int id)
+        {
+            if (_instance == null)
+            {
+               lock (_lock)
+                {
+                    if(_instance == null) 
+                    {
+                        _instance = new UploadService(); 
+                        _instance.Id = id;
+                    }                   
+                }              
+            }
+            return _instance;
+        }
+    }
+}
